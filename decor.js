c04f7f3d9d2cc4308185dd9b1dba3649bcbd72e0
DinhKhoi22 <111755052+DinhKhoi22@users.noreply.github.com>
2023-01-24 20:26:28 +0700

    okokok

--- decor.js
+++ decor.js
@@ -1,1 +1,24 @@
+var header = document.getElementById('header');
+var mobileMenu = document.getElementById('mobile-menu');
+var headerHeight = header.clientHeight;
+
+mobileMenu.onclick = function(){
+    var isClosed = header.clientHeight === headerHeight;
+    if(isClosed){
+        header.style.height = '300px'
+    }
+    else{
+        header.style.height = null
+    }
+}
+
+
+var menuItems = document.querySelectorAll('#navbar li a[href*="#"]');
+for (var i = 0; i<menuItems.length; i++){
+    var menuItem = menuItems[i];
+
+    menuItem.onclick = function(){
+        header.style.height = null
+    }
+}
 
